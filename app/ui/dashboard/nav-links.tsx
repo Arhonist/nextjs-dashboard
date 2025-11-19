8db0e1a5f220a6bc40c3f47a8004904c8e7ccf20
Nikita Aushev <arhonist@gmail.com>
2025-11-20 01:07:27 +0500

    Стриминг асинхронных подкомпонентов домашней страницы дашборда.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,13 +8,13 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-    { name: 'Home', href: '/dashboard', icon: HomeIcon },
+    { name: 'Домой', href: '/dashboard', icon: HomeIcon },
     {
-        name: 'Invoices',
+        name: 'Инвойсы',
         href: '/dashboard/invoices',
         icon: DocumentDuplicateIcon
     },
-    { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon }
+    { name: 'Покупатели', href: '/dashboard/customers', icon: UserGroupIcon }
 ];
 
 export default function NavLinks() {
