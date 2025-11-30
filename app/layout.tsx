300f47888d867843ac1a0360eca7e88ed5801b33
Nikita Aushev <arhonist@gmail.com>
2025-12-01 01:06:38 +0500

    Добавление метаданных для корневого layout и постранично.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Дашборд',
+        default: 'Дашборд'
+    },
+    description: 'Дашборд по официальному курсу Next.js от Vercel, использует App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
