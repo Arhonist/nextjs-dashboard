f3765c821b37cbaf3089246eb4c170cd16cd3c33
Nikita Aushev <arhonist@gmail.com>
2025-12-01 00:51:17 +0500

    Система аутентификации на основе next-auth. Соответствующие доработки компонентнов и действий.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -87,10 +89,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   `;
     } catch (error) {
         console.log(error);
-
-        return {
-            message: 'Database Error: Failed to updateInvoice.'
-        };
     }
 
     revalidatePath('/dashboard/invoices');
@@ -102,11 +100,23 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
         console.log(error);
-
-        return {
-            message: 'Database Error: Failed to deleteInvoice.'
-        };
     }
 
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Неправильные логин / пароль.';
+                default:
+                    return 'Что-то пошло не так.';
+            }
+        }
+        throw error;
+    }
 }
